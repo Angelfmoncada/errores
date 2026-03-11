b6ea43b53794cda3cb43a68e4d15737d472c458a
KimJoonie <kimn_v@unicah.edu>
2026-03-11 16:37:45 -0600

    Agregar usuarios con roles y casos de prueba

--- ErroresProyecto/src/Script_BD.sql
+++ ErroresProyecto/src/Script_BD.sql
@@ -17,7 +17,10 @@ CREATE TABLE usuarios (
     id INT(11) NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(64) NOT NULL,
+    rol VARCHAR(20) NOT NULL DEFAULT 'admin',
     PRIMARY KEY (id)
 );
 
-INSERT INTO usuarios (username, password) VALUES ('admin', '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9');
+INSERT INTO usuarios (username, password, rol) VALUES ('admin', '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9', 'admin');
+INSERT INTO usuarios (username, password, rol) VALUES ('kimberly', 'e3a389af866e078766ced912ed722c067d11a15940c9905c7304487aec6318a6', 'admin');
+INSERT INTO usuarios (username, password, rol) VALUES ('visual', '2b627fca21406f02e065ad7987bc7641a6a9a61b3902f852f67b733e2792727b', 'visual');
